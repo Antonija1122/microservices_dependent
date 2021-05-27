6a349ab686bf6b40a6b530eee289975662bdbb27
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 16:01:21 +0200

    initial commit travis add deploy added namespaces travis

--- pipeline_scripts/push_artifact.sh
+++ pipeline_scripts/push_artifact.sh
@@ -1,2 +1,5 @@
+docker build -t antonijaa/service1:test -f ./service1/Dockerfile ./service1
+docker build -t antonijaa/service2:test -f ./service2/Dockerfile ./service2
+
 docker push antonijaa/service1:test
 docker push antonijaa/service2:test
