299aa79d8ed96761a3832641136850477c7167ee
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 16:24:24 +0200

    initial commit travis add deploy added namespaces travis

--- pipeline_scripts/push_artifact.sh
+++ pipeline_scripts/push_artifact.sh
@@ -1,5 +1,5 @@
 docker build -t antonijaa/service1:test -f ./service1/Dockerfile ./service1
 docker build -t antonijaa/service2:test -f ./service2/Dockerfile ./service2
-
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push antonijaa/service1:test
 docker push antonijaa/service2:test
