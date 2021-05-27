0a8afa35ac2c8efe66fc690418a8523970fc6150
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 21:52:41 +0200

    redundant but works added unique tag to images

--- pipeline_scripts/deploy_to_staging.sh
+++ pipeline_scripts/deploy_to_staging.sh
@@ -12,3 +12,9 @@ kubectl apply -f ./k8s/service1-deployment.yaml  --namespace=project2-stg
 kubectl apply -f ./k8s/service2-cluster-service-ip.yaml  --namespace=project2-stg
 kubectl apply -f ./k8s/service2-deployment.yaml  --namespace=project2-stg
 kubectl apply -f ./k8s/ingress/ingress-service-project2-stg.yaml  --namespace=project2-stg
+
+#impertive set image
+kubectl set image deployments/service1-deployment service1=antonijaa/service1:$TAG --namespace=project1-stg
+kubectl set image deployments/service1-deployment service1=antonijaa/service1:$TAG --namespace=project2-stg
+kubectl set image deployments/service2-deployment service1=antonijaa/service2:$TAG --namespace=project1-stg
+kubectl set image deployments/service2-deployment service1=antonijaa/service2:$TAG --namespace=project2-stg
--- pipeline_scripts/push_artifact.sh
+++ pipeline_scripts/push_artifact.sh
@@ -1,5 +1,10 @@
-docker build -t antonijaa/service1:test -f ./service1/Dockerfile ./service1
-docker build -t antonijaa/service2:test -f ./service2/Dockerfile ./service2
+docker build -t antonijaa/service1:latest -t antonijaa/service1:$TAG -f ./service1/Dockerfile ./service1
+docker build -t antonijaa/service2:latest -t antonijaa/service2:$TAG -f ./service2/Dockerfile ./service2
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push antonijaa/service1:test
-docker push antonijaa/service2:test
+docker push antonijaa/service1:latest
+docker push antonijaa/service2:latest
+
+docker push antonijaa/service1:$TAG
+docker push antonijaa/service2:$TAG
+
