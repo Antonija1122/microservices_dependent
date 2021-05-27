369737c58b674b853f53c0b546facf6b12724b98
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 18:22:09 +0200

    prod dev env

---
+++ pipeline_scripts/deploy_to_production.sh
@@ -0,0 +1,14 @@
+#projekti, ista stvar bi bila za razlicite env npr. --namespace=project1-dev
+kubectl apply -f project1-prod.json
+kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project1-prod
+kubectl apply -f k8s/service1-deployment.yaml --namespace=project1-prod
+kubectl apply -f k8s/service2-cluster-service-ip.yaml --namespace=project1-prod
+kubectl apply -f k8s/service2-deployment.yaml  --namespace=project1-prod
+kubectl apply -f k8s/ingress/ingress-service-project1-prod.yaml  --namespace=project1-prod
+
+kubectl apply -f project2-prod.json
+kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project2-prod
+kubectl apply -f k8s/service1-deployment.yaml  --namespace=project2-prod
+kubectl apply -f k8s/service2-cluster-service-ip.yaml  --namespace=project2-prod
+kubectl apply -f k8s/service2-deployment.yaml  --namespace=project2-prod
+kubectl apply -f k8s/ingress/ingress-service-project2-prod.yaml  --namespace=project2-prod
