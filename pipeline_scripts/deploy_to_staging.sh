1357a3bed46bac81ddffc2d93aeb3cedb8ee4d25
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 18:41:46 +0200

    prod dev env

--- pipeline_scripts/deploy_to_staging.sh
+++ pipeline_scripts/deploy_to_staging.sh
@@ -1,14 +1,14 @@
 #projekti, ista stvar bi bila za razlicite env npr. --namespace=project1-dev
-kubectl apply -f k8s/namespaces/project1-stg.json
-kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project1-stg
-kubectl apply -f k8s/service1-deployment.yaml --namespace=project1-stg
-kubectl apply -f k8s/service2-cluster-service-ip.yaml --namespace=project1-stg
-kubectl apply -f k8s/service2-deployment.yaml  --namespace=project1-stg
-kubectl apply -f k8s/ingress/ingress-service-project1-stg.yaml  --namespace=project1-stg
+kubectl apply -f ./k8s/namespaces/project1-stg.json
+kubectl apply -f ./k8s/service1-cluster-service-ip.yaml --namespace=project1-stg
+kubectl apply -f ./k8s/service1-deployment.yaml --namespace=project1-stg
+kubectl apply -f ./k8s/service2-cluster-service-ip.yaml --namespace=project1-stg
+kubectl apply -f ./k8s/service2-deployment.yaml  --namespace=project1-stg
+kubectl apply -f ./k8s/ingress/ingress-service-project1-stg.yaml  --namespace=project1-stg
 
-kubectl apply -f k8s/namespaces/project2-stg.json
-kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project2-stg
-kubectl apply -f k8s/service1-deployment.yaml  --namespace=project2-stg
-kubectl apply -f k8s/service2-cluster-service-ip.yaml  --namespace=project2-stg
-kubectl apply -f k8s/service2-deployment.yaml  --namespace=project2-stg
-kubectl apply -f k8s/ingress/ingress-service-project2-stg.yaml  --namespace=project2-stg
+kubectl apply -f ./k8s/namespaces/project2-stg.json
+kubectl apply -f ./k8s/service1-cluster-service-ip.yaml --namespace=project2-stg
+kubectl apply -f ./k8s/service1-deployment.yaml  --namespace=project2-stg
+kubectl apply -f ./k8s/service2-cluster-service-ip.yaml  --namespace=project2-stg
+kubectl apply -f ./k8s/service2-deployment.yaml  --namespace=project2-stg
+kubectl apply -f ./k8s/ingress/ingress-service-project2-stg.yaml  --namespace=project2-stg
