2a2860fb8bf36c060b10199542d4c79c5a1d40f5
Antonija1122 <antonija.cesic1122@gmail.com>
2021-05-27 18:32:08 +0200

    prod dev env

--- pipeline_scripts/deploy_to_production.sh
+++ pipeline_scripts/deploy_to_production.sh
@@ -6,7 +6,7 @@ kubectl apply -f k8s/service2-cluster-service-ip.yaml --namespace=project1-prod
 kubectl apply -f k8s/service2-deployment.yaml  --namespace=project1-prod
 kubectl apply -f k8s/ingress/ingress-service-project1-prod.yaml  --namespace=project1-prod
 
-kubectl apply -f project2-prod.json
+kubectl apply -f k8s/namespaces/project2-prod.json
 kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project2-prod
 kubectl apply -f k8s/service1-deployment.yaml  --namespace=project2-prod
 kubectl apply -f k8s/service2-cluster-service-ip.yaml  --namespace=project2-prod
--- pipeline_scripts/deploy_to_staging.sh
+++ pipeline_scripts/deploy_to_staging.sh
@@ -6,7 +6,7 @@ kubectl apply -f k8s/service2-cluster-service-ip.yaml --namespace=project1-stg
 kubectl apply -f k8s/service2-deployment.yaml  --namespace=project1-stg
 kubectl apply -f k8s/ingress/ingress-service-project1-stg.yaml  --namespace=project1-stg
 
-kubectl apply -f project2-stg.json
+kubectl apply -f k8s/namespaces/project2-stg.json
 kubectl apply -f k8s/service1-cluster-service-ip.yaml --namespace=project2-stg
 kubectl apply -f k8s/service1-deployment.yaml  --namespace=project2-stg
 kubectl apply -f k8s/service2-cluster-service-ip.yaml  --namespace=project2-stg
